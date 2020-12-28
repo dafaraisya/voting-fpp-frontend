22f993aca6631e1061c6b558cad7c05360295240
Dafa Raisya <dafaraisya.dr@gmail.com>
2020-12-28 19:10:56 +0700

    solve timeline error

--- src/views/vote/Announcement.vue
+++ src/views/vote/Announcement.vue
@@ -70,7 +70,7 @@ export default {
           type: "bar",
         },
         title: {
-          text: "Hasil Perhitungan Suara PEMIRA FMIPA UNS 2020",
+          text: "Hasil Perhitungan Suara PEMIRA FPP UNDIP 2021",
         },
         xAxis: {
           categories: ["oke", "oke2", "oke3"],
